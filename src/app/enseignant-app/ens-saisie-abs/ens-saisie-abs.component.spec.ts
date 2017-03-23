a449cecc9ddcb794a61601b5948b1d00f1905f56
Felloula <df_tata@esi.dz>
2017-03-23 20:56:19 +0100

    v2.2
    v1.2

---
+++ src/app/enseignant-app/ens-saisie-abs/ens-saisie-abs.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EnsSaisieAbsComponent } from './ens-saisie-abs.component';
+
+describe('EnsSaisieAbsComponent', () => {
+  let component: EnsSaisieAbsComponent;
+  let fixture: ComponentFixture<EnsSaisieAbsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EnsSaisieAbsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EnsSaisieAbsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
