ddbcc4331f99e4a374101b96e084fe529805fd19
Felloula <df_tata@esi.dz>
2017-03-23 19:11:56 +0100

    v1.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { Md2Module } from 'md2';
+
 
 import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
-
-
-
 import { AppComponent } from './app.component';
 import { EnseignantAppComponent } from './enseignant-app/enseignant-app.component';
 import { EtudiantAppComponent } from './etudiant-app/etudiant-app.component';
 
 import  { EnseignantAppModule } from './enseignant-app/enseignant-app.module';
+
+import { AddEvalComponent } from './enseignant-app/ens-gest-eval/add-eval/add-eval.component';
+import { DelEvalComponent } from './enseignant-app/ens-gest-eval/del-eval/del-eval.component';
+import { ModifEvalComponent } from './enseignant-app/ens-gest-eval/modif-eval/modif-eval.component';
 
 import {routes} from './app.routes'
 
@@ -29,8 +32,11 @@ import {routes} from './app.routes'
     FormsModule,
     HttpModule,
     routes,
-    EnseignantAppModule
+    EnseignantAppModule,
+    Md2Module
   ],
+  entryComponents: [ AddEvalComponent,DelEvalComponent,ModifEvalComponent],
+ 
   providers: [],
   bootstrap: [AppComponent]
   
